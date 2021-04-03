5ca5b18c9456522abdd0205e14dde9630009e0af
ALEX3KOVAL <62327917+ALEX3KOVAL@users.noreply.github.com>
2021-04-03 23:03:28 +0300

    Лабораторная работа_2_задание_1
    Выполнено задание 1 с применением методов модуля Enumerable

---
+++ lab2_kovalenko_option4.rb
@@ -0,0 +1,26 @@
+=begin
+Задание 1
+=end
+
+def min_among_elements(list)
+	list.min
+end
+
+def max_among_elements(list)
+	list.max
+end
+
+def summ_of_elements(list)
+	list.inject(0) {|sum, x| sum + x}
+end
+
+def product_of_elements(list)
+	list.inject(1) {|sum, x| sum * x}
+end
+
+lt = gets.chomp.split.map! {|elem| elem.to_i}
+
+puts "Минимальный элемент введённого списка равен -- #{min_among_elements(lt)}"
+puts "Максимальный элемент введённого списка равен -- #{max_among_elements(lt)}"
+puts "Сумма всех элементов списка -- #{summ_of_elements(lt)}"
+print "Произведение всех элементов списка -- #{product_of_elements(lt)}"
