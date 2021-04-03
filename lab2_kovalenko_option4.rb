a8ad38994f236ca1fbc2d6cc0806176fe6006592
ALEX3KOVAL <62327917+ALEX3KOVAL@users.noreply.github.com>
2021-04-04 02:36:28 +0300

    Лабораторная работа_2_задание_2
    Выполнено задание 2 с применением методов класса Array

--- lab2_kovalenko_option4.rb
+++ lab2_kovalenko_option4.rb
@@ -24,3 +24,20 @@ puts "Минимальный элемент введённого списка р
 puts "Максимальный элемент введённого списка равен -- #{max_among_elements(lt)}"
 puts "Сумма всех элементов списка -- #{summ_of_elements(lt)}"
 print "Произведение всех элементов списка -- #{product_of_elements(lt)}"
+
+=begin
+Задание 2
+=end
+
+def new_summ_of_elements(list)
+	list.sum
+end
+
+def new_product_of_elements(list)
+	list.reduce(:*)
+end
+
+lt = gets.chomp.split.map! {|elem| elem.to_i}
+
+puts "Сумма всех элементов списка -- #{new_summ_of_elements(lt)}"
+print "Произведение всех элементов списка -- #{new_product_of_elements(lt)}"
